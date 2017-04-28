6cdd6438995b9889712c3e5869e4c9adb7193728
Tomoka Baba <baba@tofulab.jp>
2017-04-28 23:42:52 +0900

    Add option for timeline limit.

--- src/Commands.php
+++ src/Commands.php
@@ -8,32 +8,35 @@ use Symfony\Component\Yaml\Yaml;
 
 class Commands
 {
-    public static function execute($cmd)
+    public static function execute($cmd, $options)
     {
         $self = new ReflectionClass('Commands');
         $methods = $self->getMethods(ReflectionMethod::IS_PRIVATE);
         foreach ($methods as $m) {
             if ($m->name === $cmd) {
-                self::$cmd();
+                self::$cmd($options);
                 return;
             }
         }
         echo "'$cmd' is invalid...";
     }
 
-    private static function auth()
+    private static function auth($options)
     {
         $authorization = new Authorization();
         $authorization->authorize();
     }
 
-    private static function timeline()
+    private static function timeline($options)
     {
         $obj = Yaml::parse(file_get_contents(__DIR__.'/../config/secret.yml'));
         $client = new \GuzzleHttp\Client();
         $res = $client->request('GET', "https://{$obj['host']}/api/v1/timelines/home", [
             'headers' => [
                 'Authorization' => "Bearer {$obj['access_token']}",
+            ],
+            'query' => [
+                'limit' => count($options) ? $options[0] : 20,
             ],
         ]);
         $timeLines = json_decode($res->getBody());
@@ -45,5 +48,10 @@ class Commands
             $msg = View::color((empty($displayName) ? $userName : $displayName),'33').PHP_EOL . View::color(preg_replace($pattern, '', $content));
             View::puts($msg);
         }
+    }
+
+    private static function toot($options)
+    {
+        // TODO
     }
 }
